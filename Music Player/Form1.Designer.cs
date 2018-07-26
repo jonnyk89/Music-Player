22efbb8539d59800a7faa826abf267d638015332
jonnyk89 <ioankrumov@gmail.com>
2018-07-26 04:09:57 +0300

    Full Project added
    Music Player, capable of playing one track at a time. Lists are not
    available (yet).

---
+++ Music Player/Form1.Designer.cs
@@ -0,0 +1,163 @@
+﻿namespace Music_Player
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.MediaPlayer = new AxWMPLib.AxWindowsMediaPlayer();
+            this.btnBrowse = new System.Windows.Forms.Button();
+            this.btnPlay = new System.Windows.Forms.Button();
+            this.btnStop = new System.Windows.Forms.Button();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.txtbLocation = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.chkbReplay = new System.Windows.Forms.CheckBox();
+            ((System.ComponentModel.ISupportInitialize)(this.MediaPlayer)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // MediaPlayer
+            // 
+            this.MediaPlayer.Enabled = true;
+            this.MediaPlayer.Location = new System.Drawing.Point(0, -1);
+            this.MediaPlayer.Name = "MediaPlayer";
+            this.MediaPlayer.OcxState = ((System.Windows.Forms.AxHost.State)(resources.GetObject("MediaPlayer.OcxState")));
+            this.MediaPlayer.Size = new System.Drawing.Size(648, 145);
+            this.MediaPlayer.TabIndex = 0;
+            // 
+            // btnBrowse
+            // 
+            this.btnBrowse.BackColor = System.Drawing.SystemColors.ButtonShadow;
+            this.btnBrowse.FlatStyle = System.Windows.Forms.FlatStyle.System;
+            this.btnBrowse.Font = new System.Drawing.Font("Wingdings", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(2)));
+            this.btnBrowse.Location = new System.Drawing.Point(256, 160);
+            this.btnBrowse.Name = "btnBrowse";
+            this.btnBrowse.Size = new System.Drawing.Size(120, 39);
+            this.btnBrowse.TabIndex = 2;
+            this.btnBrowse.Text = "1";
+            this.btnBrowse.UseVisualStyleBackColor = false;
+            this.btnBrowse.Click += new System.EventHandler(this.btnBrowse_Click);
+            // 
+            // btnPlay
+            // 
+            this.btnPlay.BackColor = System.Drawing.SystemColors.ButtonShadow;
+            this.btnPlay.FlatStyle = System.Windows.Forms.FlatStyle.System;
+            this.btnPlay.Font = new System.Drawing.Font("Webdings", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(2)));
+            this.btnPlay.Location = new System.Drawing.Point(0, 111);
+            this.btnPlay.Name = "btnPlay";
+            this.btnPlay.Size = new System.Drawing.Size(59, 33);
+            this.btnPlay.TabIndex = 3;
+            this.btnPlay.Text = ";";
+            this.btnPlay.UseVisualStyleBackColor = false;
+            this.btnPlay.Click += new System.EventHandler(this.btnPlay_Click);
+            // 
+            // btnStop
+            // 
+            this.btnStop.BackColor = System.Drawing.SystemColors.ButtonShadow;
+            this.btnStop.FlatStyle = System.Windows.Forms.FlatStyle.System;
+            this.btnStop.Font = new System.Drawing.Font("Webdings", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(2)));
+            this.btnStop.Location = new System.Drawing.Point(56, 111);
+            this.btnStop.Name = "btnStop";
+            this.btnStop.Size = new System.Drawing.Size(51, 33);
+            this.btnStop.TabIndex = 5;
+            this.btnStop.Text = "g";
+            this.btnStop.UseVisualStyleBackColor = false;
+            this.btnStop.Click += new System.EventHandler(this.btnStop_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // txtbLocation
+            // 
+            this.txtbLocation.Location = new System.Drawing.Point(12, 217);
+            this.txtbLocation.Name = "txtbLocation";
+            this.txtbLocation.ReadOnly = true;
+            this.txtbLocation.Size = new System.Drawing.Size(623, 20);
+            this.txtbLocation.TabIndex = 7;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.label1.Location = new System.Drawing.Point(12, 201);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(107, 13);
+            this.label1.TabIndex = 8;
+            this.label1.Text = "Current File Location:";
+            // 
+            // chkbReplay
+            // 
+            this.chkbReplay.Appearance = System.Windows.Forms.Appearance.Button;
+            this.chkbReplay.AutoSize = true;
+            this.chkbReplay.Font = new System.Drawing.Font("Webdings", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(2)));
+            this.chkbReplay.Location = new System.Drawing.Point(600, 113);
+            this.chkbReplay.Name = "chkbReplay";
+            this.chkbReplay.Size = new System.Drawing.Size(35, 29);
+            this.chkbReplay.TabIndex = 9;
+            this.chkbReplay.Text = "q";
+            this.chkbReplay.UseVisualStyleBackColor = true;
+            this.chkbReplay.CheckedChanged += new System.EventHandler(this.chkbReplay_CheckedChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.InactiveCaptionText;
+            this.ClientSize = new System.Drawing.Size(647, 249);
+            this.Controls.Add(this.chkbReplay);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtbLocation);
+            this.Controls.Add(this.btnStop);
+            this.Controls.Add(this.btnPlay);
+            this.Controls.Add(this.btnBrowse);
+            this.Controls.Add(this.MediaPlayer);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Music Player";
+            ((System.ComponentModel.ISupportInitialize)(this.MediaPlayer)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private AxWMPLib.AxWindowsMediaPlayer MediaPlayer;
+        private System.Windows.Forms.Button btnBrowse;
+        private System.Windows.Forms.Button btnPlay;
+        private System.Windows.Forms.Button btnStop;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.TextBox txtbLocation;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.CheckBox chkbReplay;
+    }
+}
+
---
+++ Music Player/Form1.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Music_Player
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            btnPlay.Text = "4";
+        }
+
+        private void btnPlay_Click(object sender, EventArgs e)
+        {
+            if (btnPlay.Text == "4") // playing
+            {
+                MediaPlayer.Ctlcontrols.play();
+                btnPlay.Text = ";";
+            }
+            else if (btnPlay.Text == ";") // paused
+            {
+                MediaPlayer.Ctlcontrols.pause();
+                btnPlay.Text = "4";
+            }
+        }
+
+        private void btnStop_Click(object sender, EventArgs e)
+        {
+            MediaPlayer.Ctlcontrols.stop();
+
+        }
+
+        private void chkbReplay_CheckedChanged(object sender, EventArgs e)
+        {
+            if (chkbReplay.Checked == true)
+            {
+                MediaPlayer.settings.setMode("loop", true);
+            }
+            else if (chkbReplay.Checked == false)
+            {
+                MediaPlayer.settings.setMode("loop", false);
+            }
+
+            if (MediaPlayer.playState == WMPLib.WMPPlayState.wmppsStopped)
+            {
+                MediaPlayer.Ctlcontrols.play();
+            }
+        }
+
+        private void btnBrowse_Click(object sender, EventArgs e)
+        {
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                txtbLocation.Text = openFileDialog1.FileName;
+            }
+
+            MediaPlayer.URL = txtbLocation.Text;
+
+            btnPlay.Text = ";";
+        }
+    }
+}
